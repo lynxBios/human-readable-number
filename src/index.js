31cb958f345fd72a5a5d25313d394eac5c43aef0
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-10-27 17:49:05 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const numbersToTwelve = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
+    if (number < 20) {
+      return numbersToTwelve[number];
+    } else if (number < 100) {
+      const ten = Math.floor(number / 10);
+      const unit = number % 10;
+      return tens[ten] + (unit > 0 ? ' ' + numbersToTwelve[unit] : '');
+    } else {
+      const hundred = Math.floor(number / 100);
+      const remainder = number % 100;
+      return numbersToTwelve[hundred] + ' hundred' + (remainder > 0 ? ' ' + toReadable(remainder) : '');
+    }
+  }
